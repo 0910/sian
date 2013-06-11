7636bca42d81050f506d3a0c5363bb206a12fcec
Octavio <octanks@hotmail.com>
2013-06-11 14:07:26 -0300

    Filtro de propiedades en el sitio

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,16 @@
 class PropertiesController < ApplicationController
   def index
-    @properties = Property.order('created_at desc').all
+    @type_of_property = params[:type_of_property]
+    @country = params[:country]
+    @location = params[:location]
+    @type_of_operation = params[:type_of_operation]
+
+  	if @type_of_property
+    	@properties = Property.where(:type_of_property => @type_of_property).where(:country => @country).where(:location => @location).where(:type_of_operation => @type_of_operation)
+  	else	
+    	@properties = Property.order('created_at desc').all
+  	end
+    # if defined?(params[:type_of_property])
   end
   
   def show
