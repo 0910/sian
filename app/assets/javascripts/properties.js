2f1a19949a090fcdf027eae07978d723b354828f
Octavio <octanks@hotmail.com>
2013-06-19 14:05:06 -0300

    filtros de tipo de propiedad en home y propiedades

---
+++ app/assets/javascripts/properties.js
@@ -0,0 +1,20 @@
+$(function(){
+	type_of_property = $('input#type_of_property').val();
+	
+	switch(type_of_property){
+		case 'Luxury Properties':
+			$('a.luxury').css({'background-position':'24px -127px'}); 
+			break;
+		case 'Urban Properties':
+			$('a.urban').css({'background-position':'-148px -127px'});   
+			break;
+		case 'Commercial Properties':
+			$('a.commercial').css({'background-position':'-322px -127px'});
+			break;
+		case 'Campos Country-Clubs':
+			$('a.campos').css({'background-position':'-672px -127px'});
+			break;
+		case 'Desarollos Inmobiliarios':
+			$('a.developments').css({'background-position':'-499px -127px'})
+	}
+});
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,24 +1,24 @@
 class PropertiesController < ApplicationController
   def index
-  	if params[:type_of_property] == 'Todos'
-  		@type_of_property = 0
+  	if params[:type_of_property] == nil
+  		@type_of_property = 'Luxury Properties'
   	else
 	    @type_of_property = params[:type_of_property]
 	  end
 
-    if params[:country] == 'Todos'
+    if params[:country] == 'Todos los paises' || params[:country] == 'Seleccione el pais'|| params[:country] == nil 
     	@country = 0
     else
 	    @country = params[:country]
 	  end
 
-	  if params[:location] == 'Todos'
+	  if params[:location] == 'Todos los barrios' || params[:location] == 'Seleccione el barrio' || params[:location] == nil 
 	  	@location = 0
 	  else
 	    @location = params[:location]
 	  end
 
-	  if params[:type_of_operation] == 'Todos'
+	  if params[:type_of_operation] == 'Todas las operaciones' || params[:type_of_operation] == 'Seleccione tipo de operacion' || params[:type_of_operation] == nil 
 	  	@type_of_operation = 0
 	  else
 	    @type_of_operation = params[:type_of_operation]
